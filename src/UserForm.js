4a1248244114c30c3cbd2c5ba97779464b1da8c4
ctoloczko <celina.toloczko@gmail.com>
2025-03-11 19:01:14 +0100

    styled form

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,25 +1,53 @@
 import { useState } from "react";
+import "./App.css";
 
 export default function UserForm() {
-  const [inputValue, setInputValue] = useState(""); // State for input value
-
-  function handleChange(event) {
-    setInputValue(event.target.value); // Update state when user types
-  }
-
   return (
     <div>
-      <p>Fyll i några ord och låt AI skapa en vacker dikt åt dig</p>
-      <label>
-        Text input:
-        <input
-          type="text"
-          name="myInput"
-          value={inputValue}
-          onChange={handleChange}
-        />
-      </label>
-      <p>Du skrev: {inputValue}</p> {/* Display user input */}
+      <h2>Fyll i några ord och låt AI skapa en vacker dikt åt dig</h2>
+      <div className="Options">
+        <label>
+          Nyckelord (t.ex. kärlek, vinter, ensamhet)
+          <input type="text" name="myInput" />
+        </label>
+      </div>
+      <div className="Options">
+        <select>
+          {" "}
+          Diktstil
+          <option value="-">-</option>
+          <option value="haiku">Haiku</option>
+          <option value="rimmande">Rimmande</option>
+          <option value="fritt">Fritt versmått</option>
+          <option value="sonett">Sonett</option>
+        </select>
+      </div>
+      <div className="Options">
+        <select>
+          {" "}
+          Stämning
+          <option value="-">-</option>
+          <option value="romantisk">Romantisk</option>
+          <option value="melankolisk">Melankolisk</option>
+          <option value="upplyftande">Upplyftande</option>
+          <option value="moerk">Mörk</option>
+        </select>
+      </div>
+
+      <div className="Options">
+        <select>
+          {" "}
+          Längd
+          <option value="-">-</option>
+          <option value="kort">Kort</option>
+          <option value="medel">Medel</option>
+          <option value="long">Lång</option>
+        </select>
+      </div>
+
+      <button>Låt oss skapa!</button>
+
+      <p>Din poem</p>
     </div>
   );
 }
