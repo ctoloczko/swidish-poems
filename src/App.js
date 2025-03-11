c3a40f9c1677f39f2f4d6d960e45ec59c925beba
ctoloczko <celina.toloczko@gmail.com>
2025-03-11 17:36:29 +0100

    added form component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import UserForm from "./UserForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Swedish
-        </a>
+        <h2>Skriv en svensk dikt med AI</h2>
+        <p>Fyll i några ord och låt AI skapa en vacker dikt åt dig</p>
       </header>
+
+      <UserForm />
     </div>
   );
 }
